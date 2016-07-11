28e8bff6e41876f5d393cb5b5bb975e1e5c89693
Andreas Wittig <andreas@widdix.de>
2016-07-11 10:00:18 +0200

    fixed async for each

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
 var redis = require("redis");
+var async = require("async");
 
-function handler(event, context, callback) {
+function handler(event, context, globalCallback) {
     console.log(JSON.stringify(event));
 
     var redisClient = redis.createClient({host: "ser-el-1nole16f83cy5.udcuwc.0001.use1.cache.amazonaws.com"});
 
-    event.Records.forEach(function(record) {
+    async.each(event.Records, function(record, callback) { 
         var key = record.dynamodb.Keys.Id.S;
-    	if (record.eventName === "INSERT" || record.eventName === "MODIFY") {
-    		var value = JSON.stringify(record.dynamodb.NewImage);
-    		console.log("Updating cache: " + key + ": " + value);
-    		redisClient.set(key, value, function(err) {
-    			if(err) {
-    				callback(err);
-    			} else {
-    				redisClient.quit();
-    				callback(null, "DONE");
-    			}
-    		});
-    	} else if (record.eventName === "REMOVE") {
-    		console.log("Deleting cache: " + key);
-    		redisClient.del(key, function(err) {
-    			if(err) {
-    				callback(err);
-    			} else {
-    				redisClient.quit();
-    				callback(null, "DONE");
-    			}
-    		});
-    	}
+        if (record.eventName === "INSERT" || record.eventName === "MODIFY") {
+            var value = JSON.stringify(record.dynamodb.NewImage);
+            console.log("Updating cache: " + key + ": " + value);
+            redisClient.set(key, value, function(err) {
+                if(err) {
+                    callback(err);
+                } else {
+                    redisClient.quit();
+                    callback();
+                }
+            });
+        } else if (record.eventName === "REMOVE") {
+            console.log("Deleting cache: " + key);
+            redisClient.del(key, function(err) {
+                if(err) {
+                    callback(err);
+                } else {
+                    redisClient.quit();
+                    callback();
+                }
+            });
+        }
+    }, function(err){
+        if(err) {
+            console.log("Error " + err);
+            globalCallback(err);
+        } else {
+            globalCallback(null, "DONE");
+        }
     });
 
     redisClient.on("error", function (err) {
